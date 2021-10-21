a9b293f959eacfc2a0bc7594c5180168d22e275d
tamumisa <lucky.misap@gmail.com>
2021-10-21 13:55:30 +0900

    menuページの作成

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 def index():
 	return render_template('/top.html')
 
+@app.route('/top',methods=["POST"])
+def top():
+	return render_template('/main.html')
+
 if __name__ == "__main__":
 	app.debug = True
 	app.run(port=7000)
